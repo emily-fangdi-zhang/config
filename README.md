e10d6a6cd7a52a0061effb4ea32379d0757fb135
Emily Zhang <emily.fangdi.zhang@hotmail.com>
2023-05-11 11:48:27 -0500

    initialize settings

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# config
+# emilyfdzhang config
+
+## Installation
+
+```
+$ npm i -D @emilyfdzhang/config
+```
+
+## Prettier
+
+Add the following to `package.json`.
+
+```
+{
+    "prettier": "@emilyfdzhang/config/prettier"
+}
+```
---
+++ prettier/index.js
@@ -0,0 +1,12 @@
+/* https://prettier.io/docs/en/options.html */
+
+/** @type { import('@types/prettier').Config } */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
